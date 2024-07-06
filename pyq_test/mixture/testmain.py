f766397896cd11fbe65ae697f6863cff1dd2289b
Shan Qiu <604587716@qq.com>
2024-07-06 09:33:31 +0800

    title: 	1. add spin test 	2. add cython setup.py

--- pyq_test/mixture/testmain.py
+++ pyq_test/mixture/testmain.py
@@ -4,10 +4,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 
-from qlinedit_test import *
+from qlinedit_test1 import *
 
 from test_import import *
-
+import qtawesome as qta
 
     
 
@@ -21,6 +21,24 @@ class Ui_MainwindowAdapter(QMainWindow, Ui_MainWindow):
         self.pushButton_setcombobox.clicked.connect(self.set_combo)
         self.comboBox.addItems(["neg","positive"])
         self.pushButton_size_hint.clicked.connect(self.print_sizehint)
+
+        self.animation2 = qta.Pulse(self.pushButton_spin, autostart=False)
+        pulse_icon = qta.icon('fa5s.spinner', color='green',
+                              animation=self.animation2)
+        self.pushButton_spin.setIcon(pulse_icon)
+
+        self.pushButton_spin.clicked.connect(self.spining)
+        self.is_sping = False
+
+        
+    
+    def spining(self):
+        if not self.is_sping:
+            self.animation2.start()
+            self.is_sping = True
+        else:
+            self.animation2.stop()
+            self.is_sping = False
         
     def set(self):
         self.lineEdit.setText("11")
@@ -39,7 +57,17 @@ class Ui_MainwindowAdapter(QMainWindow, Ui_MainWindow):
         print(self.label.sizeHint())
         ti = test_import()
         ti.test()
- 
+
+    def running(self):
+        pass
+        if not self.is_running:
+            self.animation2.start()
+            print("start")
+            self.is_running = True
+        else:
+            self.is_running = False
+            self.animation2.stop()
+            print("stop")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
